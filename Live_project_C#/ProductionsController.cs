6b650dea51e2ab2acb66cbf59fa5c1716e785358
Gil Furman <gilfurman88@gmail.com>
2022-08-26 11:39:13 -0400

    live project c#

---
+++ Live_project_C#/ProductionsController.cs
@@ -0,0 +1,132 @@
+﻿using PagedList;
+using PagedList.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheatreCMS3.Areas.Prod.Models;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Areas.Prod.Controllers
+{
+    public class ProductionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Prod/Productions
+        // A search for productions by title. 
+        public ActionResult Index(string searchString, int? page)
+        {
+            return View(db.Productions.Where(x => x.Description.Contains(searchString) || searchString == null).ToList().ToPagedList(page ?? 1, 2));
+        }
+         
+        // GET: Prod/Productions/Details/5
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Production production = db.Productions.Find(id);
+            if (production == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production);
+        }
+
+        // GET: Prod/Productions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Prod/Productions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductionId,Description,Playwright,Runtime,OpeningDay,ClosingDay,ShowTimeEve,ShowTimeMat,Season,IsWorldPremiere,TicketLink,IsCurrentlyShowing")] Production production)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productions.Add(production);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(production);
+        }
+
+        // GET: Prod/Productions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Production production = db.Productions.Find(id);
+            if (production == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production);
+        }
+
+        // POST: Prod/Productions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductionId,Description,Playwright,Runtime,OpeningDay,ClosingDay,ShowTimeEve,ShowTimeMat,Season,IsWorldPremiere,TicketLink,IsCurrentlyShowing")] Production production)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(production).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(production);
+        }
+
+        // GET: Prod/Productions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Production production = db.Productions.Find(id);
+            if (production == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production);
+        }
+
+        // POST: Prod/Productions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Production production = db.Productions.Find(id);
+            db.Productions.Remove(production);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
